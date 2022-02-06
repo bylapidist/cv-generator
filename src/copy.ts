a3189ceabcd77654a909c0d0c3be1fe5090c4b2f
Brett Dorrans <hello@lapidist.net>
2022-02-06 22:34:51 +0000

    fix: font paths

---
+++ src/copy.ts
@@ -0,0 +1,17 @@
+export default {
+    name: 'Brett Dorrans',
+    title: 'Senior Front End Software Engineer',
+    intro: 'Building ambitious software professionally for over a decade, I have led diverse teams of talented engineers to build cohesive, quality products with a focus on accessibility, inclusive design, and research-led UX. I have worked closely with designers, clients and stakeholders of all levels to break down requirements, align digital strategy, and grow their business. My experience has enabled me to push technology and products forward; I write clean, semantic code and deliver to the highest standard. With passion and commitment I empower teams, clients and colleagues to grow and reach their highest potential.',
+    sectionHeaders: {
+        experience: 'Experience',
+        contact: 'Contact'
+    },
+    jobs: [
+        {
+            company: 'Lapidist',
+            position: 'Director',
+            from: '',
+            to: 'Present'
+        }
+    ]
+};
--- src/pdf.tsx
+++ src/pdf.tsx
@@ -7,24 +7,29 @@ import {
     StyleSheet,
     Font
 } from '@react-pdf/renderer';
+import * as path from 'path';
 import Logo from './components/Logo';
+import copy from './copy';
 
 Font.register({
     family: 'Montserrat',
     fonts: [
         {
-            src: `./fonts/Montserrat-Regular.ttf`
+            src: `${path.join(__dirname, '../../fonts/Montserrat-Regular.ttf')}`
         },
         {
-            src: `./fonts/Montserrat-Bold.ttf`,
+            src: `${path.join(__dirname, '../../fonts/Montserrat-Bold.ttf')}`,
             fontWeight: 'bold'
         },
         {
-            src: `./fonts/Montserrat-Medium.ttf`,
+            src: `${path.join(__dirname, '../../fonts/Montserrat-Medium.ttf')}`,
             fontWeight: 'medium'
         },
         {
-            src: `./fonts/Montserrat-SemiBold.ttf`,
+            src: `${path.join(
+                __dirname,
+                '../../fonts/Montserrat-SemiBold.ttf'
+            )}`,
             fontWeight: 'semibold'
         }
     ]
@@ -35,14 +40,29 @@ const styles = StyleSheet.create({
         fontFamily: 'Montserrat',
         flexDirection: 'column',
         backgroundColor: '#FFFFFF',
-        marginHorizontal: 42,
         marginVertical: 28
     },
     name: {
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        fontSize: 14
     },
-    header: {},
-    section: {}
+    title: {
+        fontSize: 10,
+        marginBottom: 12
+    },
+    header: {
+        marginHorizontal: 42
+    },
+    section: {
+        marginHorizontal: 42,
+        fontSize: 9,
+        lineHeight: 1.5,
+        marginBottom: 12
+    },
+    sectionHeader: {
+        fontWeight: 'bold',
+        fontSize: 11
+    }
 });
 
 const PDF = () => (
@@ -50,14 +70,21 @@ const PDF = () => (
         <Page size="A4" style={styles.page}>
             <View style={styles.header}>
                 <Logo />
-                <Text style={styles.name}>Brett Dorrans</Text>
-                <Text>Senior Front End Software Engineer</Text>
+                <Text style={styles.name}>{copy.name}</Text>
+                <Text style={styles.title}>{copy.title}</Text>
             </View>
             <View style={styles.section}>
-                <Text>Experience</Text>
+                <Text>{copy.intro}</Text>
             </View>
             <View style={styles.section}>
-                <Text>Contact</Text>
+                <Text style={styles.sectionHeader}>
+                    {copy.sectionHeaders.experience}
+                </Text>
+            </View>
+            <View style={styles.section}>
+                <Text style={styles.sectionHeader}>
+                    {copy.sectionHeaders.contact}
+                </Text>
             </View>
         </Page>
     </Document>

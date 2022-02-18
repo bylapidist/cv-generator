fa3598b20c8585a36782a80533545fa8d1a99910
Brett Dorrans <hello@lapidist.net>
2022-02-18 23:48:50 +0000

    feat: jobs (#11)

--- src/copy.ts
+++ src/copy.ts
@@ -10,8 +10,106 @@ export default {
         {
             company: 'Lapidist',
             position: 'Director',
-            from: '',
-            to: 'Present'
+            from: 'Nov 2016',
+            to: 'Present',
+            description: null,
+            technologies: [
+                'React',
+                'Redux',
+                'JavaScript',
+                'TypeScript',
+                'Jest',
+                'PHP8',
+                'Doctrine',
+                'MySQL',
+                'Java',
+                'CI/CD'
+            ]
+        },
+        {
+            company: 'LendInvest',
+            position: 'Senior Software Engineer',
+            from: 'Apr 2021',
+            to: 'Present',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse maximus tincidunt dictum. Phasellus tempus enim non elementum convallis. Maecenas mollis rhoncus aliquam. Vivamus quis malesuada dolor. Phasellus a augue ac odio posuere convallis. Cras pharetra massa ac feugiat mattis. Integer blandit dui tortor, malesuada malesuada lorem feugiat et. Praesent sit amet ex bibendum, tristique justo placerat, pulvinar ex. Praesent sed tristique lectus. Morbi posuere consequat volutpat. Nam facilisis, libero id posuere dignissim, purus diam dignissim nisi, vel malesuada sem dolor sed quam. Proin aliquam pretium nisl vel feugiat.',
+            technologies: [
+                'React',
+                'Redux',
+                'JavaScript',
+                'Jest',
+                'Design Systems',
+                'CI/CD'
+            ]
+        },
+        {
+            company: 'Golden Charter',
+            position: 'Software Developer',
+            from: 'Jun 2020',
+            to: 'Apr 2021',
+            description:
+                'Built software for the funeral industry as part of the Product and Innovation team. Moved the front end architecture to a design system and component based workflow using Vue. Improved code quality through automated CI tooling, increased developer productivity through automation of release cycles and adoption of semantic versioning, and modernised legacy C# applications. Pushed for a focus on accessibility and inclusive design principles, and enjoyed mentoring junior members of the team on best practises.',
+            technologies: [
+                'Vue',
+                'Vuex',
+                'Jest',
+                'JavaScript',
+                'PHP7',
+                'Doctrine',
+                'MySQL',
+                'C#',
+                'CI/CD'
+            ]
+        },
+        {
+            company: 'Golden Charter',
+            position: 'Software Consultant (Contract)',
+            from: 'Feb 2020',
+            to: 'Jun 3020',
+            description: null,
+            technologies: null
+        },
+        {
+            company: 'Markup',
+            position: 'Front-End Team Lead',
+            from: 'Aug 2019',
+            to: 'Dec 2019',
+            description:
+                'Built enterprise e-commerce software for industry leading fashion brands. Developed tooling, processes and design methodology for the implementation of design systems built with React, Redux, CSS Modules, and Symfony. Mentored junior developers and conducted knowledge sharing workshops and talks. Scoped project requirements and regularly travelled to Stockholm to engage with key stakeholders including designers, engineers, marketers, IT, and senior management to advance company goals, sell features, and improve client relations. Constructed sprints and set sprint goals and chaired agile rituals. Advocated for more robust tooling via automated code quality checks in CI. Evangelised an approach to managing people that views each as an individual, fostered a culture of openness, transparency, and respect.',
+            technologies: [
+                'React',
+                'Redux',
+                'CSS Modules',
+                'JavaScript',
+                'PHP7',
+                'Doctrine',
+                'MySQL',
+                'CI/CD'
+            ]
+        },
+        {
+            company: 'Markup',
+            position: 'Senior Software Engineer',
+            from: 'Aug 2019',
+            to: 'Dec 2019',
+            description: null,
+            technologies: null
+        },
+        {
+            company: 'Everyone',
+            position: 'Developer',
+            from: 'Nov 2016',
+            to: 'Dec 2017',
+            description: null,
+            technologies: ['Angular2', 'Ionic', 'JavaScript', 'PHP', 'SCSS']
+        },
+        {
+            company: 'Ibiza Digital Media',
+            position: 'Senior Digital Designer',
+            from: 'Oct 2014',
+            to: 'Sep 2016',
+            description: null,
+            technologies: ['JavaScript', 'PHP', 'SCSS']
         }
     ]
 };

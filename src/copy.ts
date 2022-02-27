f40d20b13751571e3e338ce02a53efa4547e37d6
Brett Dorrans <hello@lapidist.net>
2022-02-27 22:29:04 +0000

    feat: update for 2022 (#18)

--- src/copy.ts
+++ src/copy.ts
@@ -1,18 +1,56 @@
 export default {
     name: 'Brett Dorrans',
     title: 'Senior Front End Software Engineer',
-    intro: 'Building ambitious software professionally for over a decade, I have led diverse teams of talented engineers to build cohesive, quality products with a focus on accessibility, inclusive design, and research-led UX. I have worked closely with designers, clients and stakeholders of all levels to break down requirements, align digital strategy, and grow their business. My experience has enabled me to push technology and products forward; I write clean, semantic code and deliver to the highest standard. With passion and commitment I empower teams, clients and colleagues to grow and reach their highest potential.',
+    intro: 'Building ambitious software professionally for over a decade, I have led diverse teams of talented engineers to build cohesive, quality products with a focus on accessibility, inclusive design, and research-led UX. I have worked closely with stakeholders of all levels to break down requirements, align digital strategy, and grow their business. My experience enables me to push technology and products forward; I write clean, semantic code and deliver to the highest standard. With passion and commitment I empower teams, clients and colleagues to grow and reach their highest potential.',
     sectionHeaders: {
         experience: 'Experience',
         contact: 'Contact'
     },
+    contact: {
+        email: 'hello@lapidist.net',
+        phone: '07944166857',
+        urls: [
+            'lapidist.net',
+            'github.com/brettdorrans',
+            'github.com/bylapidist',
+            'linkedin.com/in/brettdorrans'
+        ]
+    },
     jobs: [
         {
-            company: 'Lapidist',
-            position: 'Director',
-            from: 'Nov 2016',
-            to: 'Present',
+            company: 'LendInvest',
+            positions: [
+                {
+                    title: 'Senior Software Engineer',
+                    from: 'Apr 2021',
+                    to: 'Present'
+                }
+            ],
             description: null,
+            technologies: [
+                'React',
+                'Redux',
+                'JavaScript',
+                'Flow',
+                'Jest',
+                'Design Systems',
+                'MySQL',
+                'PHP8',
+                'OpenAPI',
+                'CI/CD'
+            ]
+        },
+        {
+            company: 'Lapidist',
+            positions: [
+                {
+                    title: 'Director',
+                    from: 'Nov 2016',
+                    to: 'Present'
+                }
+            ],
+            description:
+                'Produced open source software and provided contract development services. Managed the full client lifecycle: scoping and pursuing clients, delivering on time and on budget, and ensured company is legally compliant.',
             technologies: [
                 'React',
                 'Redux',
@@ -27,28 +65,21 @@ export default {
             ]
         },
         {
-            company: 'LendInvest',
-            position: 'Senior Software Engineer',
-            from: 'Apr 2021',
-            to: 'Present',
-            description:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse maximus tincidunt dictum. Phasellus tempus enim non elementum convallis. Maecenas mollis rhoncus aliquam. Vivamus quis malesuada dolor. Phasellus a augue ac odio posuere convallis. Cras pharetra massa ac feugiat mattis. Integer blandit dui tortor, malesuada malesuada lorem feugiat et. Praesent sit amet ex bibendum, tristique justo placerat, pulvinar ex. Praesent sed tristique lectus. Morbi posuere consequat volutpat. Nam facilisis, libero id posuere dignissim, purus diam dignissim nisi, vel malesuada sem dolor sed quam. Proin aliquam pretium nisl vel feugiat.',
-            technologies: [
-                'React',
-                'Redux',
-                'JavaScript',
-                'Jest',
-                'Design Systems',
-                'CI/CD'
-            ]
-        },
-        {
             company: 'Golden Charter',
-            position: 'Software Developer',
-            from: 'Jun 2020',
-            to: 'Apr 2021',
+            positions: [
+                {
+                    title: 'Software Developer',
+                    from: 'Jun 2020',
+                    to: 'Apr 2021'
+                },
+                {
+                    title: 'Software Consultant (Contract)',
+                    from: 'Feb 2020',
+                    to: 'Jun 2020'
+                }
+            ],
             description:
-                'Built software for the funeral industry as part of the Product and Innovation team. Moved the front end architecture to a design system and component based workflow using Vue. Improved code quality through automated CI tooling, increased developer productivity through automation of release cycles and adoption of semantic versioning, and modernised legacy C# applications. Pushed for a focus on accessibility and inclusive design principles, and enjoyed mentoring junior members of the team on best practises.',
+                'Built software for the funeral industry as part of the Product and Innovation team. Moved products to a design system and component based workflow. Improved code quality through CI tooling, increased developer productivity through automation of release cycles and adoption of semver, and modernised legacy C# applications. Advocated for accessibility and inclusive design principles, and mentored junior members of the team on best practises.',
             technologies: [
                 'Vue',
                 'Vuex',
@@ -62,20 +93,21 @@ export default {
             ]
         },
         {
-            company: 'Golden Charter',
-            position: 'Software Consultant (Contract)',
-            from: 'Feb 2020',
-            to: 'Jun 3020',
-            description: null,
-            technologies: null
-        },
-        {
             company: 'Markup',
-            position: 'Front-End Team Lead',
-            from: 'Aug 2019',
-            to: 'Dec 2019',
+            positions: [
+                {
+                    title: 'Front-End Team Lead',
+                    from: 'Aug 2019',
+                    to: 'Dec 2019'
+                },
+                {
+                    title: 'Senior Software Engineer',
+                    from: 'Jan 2018',
+                    to: 'Dec 2019'
+                }
+            ],
             description:
-                'Built enterprise e-commerce software for industry leading fashion brands. Developed tooling, processes and design methodology for the implementation of design systems built with React, Redux, CSS Modules, and Symfony. Mentored junior developers and conducted knowledge sharing workshops and talks. Scoped project requirements and regularly travelled to Stockholm to engage with key stakeholders including designers, engineers, marketers, IT, and senior management to advance company goals, sell features, and improve client relations. Constructed sprints and set sprint goals and chaired agile rituals. Advocated for more robust tooling via automated code quality checks in CI. Evangelised an approach to managing people that views each as an individual, fostered a culture of openness, transparency, and respect.',
+                'Built enterprise e-commerce software for industry leading fashion brands. Developed tooling, processes and design methodology for design systems. Mentored junior developers and conducted knowledge sharing workshops and talks. Scoped project requirements and regularly travelled to Stockholm to engage with stakeholders including designers, engineers, marketers, IT, and senior management. Advanced company goals, sold features, and improved client relations. Constructed sprints and set sprint goals and chaired agile rituals. Advocated for more robust tooling via automated code quality checks in CI. Evangelised an approach to managing people that views each as an individual, fostered a culture of openness, transparency, and respect.',
             technologies: [
                 'React',
                 'Redux',
@@ -88,27 +120,29 @@ export default {
             ]
         },
         {
-            company: 'Markup',
-            position: 'Senior Software Engineer',
-            from: 'Aug 2019',
-            to: 'Dec 2019',
-            description: null,
-            technologies: null
-        },
-        {
             company: 'Everyone',
-            position: 'Developer',
-            from: 'Nov 2016',
-            to: 'Dec 2017',
-            description: null,
+            positions: [
+                {
+                    title: 'Developer',
+                    from: 'Nov 2016',
+                    to: 'Dec 2017'
+                }
+            ],
+            description:
+                'Worked with designers, project managers and clients on e-commerce and web applications. Developed a cross platform mobile app for the Go Industrial museum group. Responsible for scoping timescales and taking ownership of complex project requirements to deliver robust, detail oriented experiences on time and within budget.',
             technologies: ['Angular2', 'Ionic', 'JavaScript', 'PHP', 'SCSS']
         },
         {
             company: 'Ibiza Digital Media',
-            position: 'Senior Digital Designer',
-            from: 'Oct 2014',
-            to: 'Sep 2016',
-            description: null,
+            positions: [
+                {
+                    title: 'Senior Digital Designer',
+                    from: 'Oct 2014',
+                    to: 'Sep 2016'
+                }
+            ],
+            description:
+                'Led a diverse team of designers and developers producing bespoke web, app, and graphic design. Worked with stakeholders at all levels to grow the startup from zero in-house development to a team of three.',
             technologies: ['JavaScript', 'PHP', 'SCSS']
         }
     ]
--- src/pdf.tsx
+++ src/pdf.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import {
-    Page,
-    Text,
-    View,
-    Document,
-    StyleSheet,
-    Font
-} from '@react-pdf/renderer';
+import { Document, Font, Page, Text, View } from '@react-pdf/renderer';
 import * as path from 'path';
 import Logo from './components/Logo';
+import Position from './components/Position';
 import copy from './copy';
+import { styles } from './styles';
 
 Font.register({
     family: 'Montserrat',
@@ -35,66 +30,6 @@ Font.register({
     ]
 });
 
-const styles = StyleSheet.create({
-    page: {
-        fontFamily: 'Montserrat',
-        flexDirection: 'column',
-        backgroundColor: '#FFFFFF',
-        marginVertical: 28
-    },
-    name: {
-        fontWeight: 'bold',
-        fontSize: 14
-    },
-    title: {
-        fontSize: 10,
-        marginBottom: 12
-    },
-    header: {
-        marginHorizontal: 42
-    },
-    section: {
-        marginHorizontal: 42,
-        fontSize: 9,
-        lineHeight: 1.5,
-        marginBottom: 12
-    },
-    sectionHeader: {
-        fontWeight: 'bold',
-        fontSize: 11,
-        marginBottom: 0
-    },
-    job: {
-        marginBottom: 12,
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap'
-    },
-    position: {
-        fontWeight: 'medium',
-        fontSize: 10,
-        display: 'flex',
-        flexDirection: 'column',
-        width: '70%'
-    },
-    duration: {
-        fontWeight: 'normal',
-        fontSize: 8,
-        flexDirection: 'column'
-    },
-    description: {
-        width: '100%',
-        marginBottom: 2
-    },
-    technologies: {
-        width: '100%',
-        fontSize: 8
-    },
-    technologiesHeader: {
-        fontWeight: 'semibold'
-    }
-});
-
 const PDF = () => (
     <Document>
         <Page size="A4" style={styles.page}>
@@ -111,16 +46,15 @@ const PDF = () => (
                     {copy.sectionHeaders.experience}
                 </Text>
                 {copy.jobs.map((job) => (
-                    <View
-                        key={`${job.company} - ${job.position}`}
-                        style={styles.job}
-                    >
-                        <Text style={styles.position}>
-                            {job.company} &ndash; {job.position}
-                        </Text>
-                        <Text style={styles.duration}>
-                            {job.from}&ndash;{job.to}
-                        </Text>
+                    <View key={job.company} style={styles.job}>
+                        {job.positions.map((position, index) => (
+                            <Position
+                                key={`${job.company} ${position.title}`}
+                                job={job}
+                                position={position}
+                                primary={index === 0}
+                            />
+                        ))}
                         {job.description && (
                             <Text style={styles.description}>
                                 {job.description}
@@ -141,6 +75,16 @@ const PDF = () => (
                 <Text style={styles.sectionHeader}>
                     {copy.sectionHeaders.contact}
                 </Text>
+                <View style={styles.contact}>
+                    <Text style={styles.primaryContact}>
+                        {`${copy.contact.email}\n${copy.contact.phone}`}
+                    </Text>
+                    <Text style={styles.secondaryContact}>
+                        {copy.contact.urls.map((url) => (
+                            <Text key={url}>{`${url}\n`}</Text>
+                        ))}
+                    </Text>
+                </View>
             </View>
         </Page>
     </Document>

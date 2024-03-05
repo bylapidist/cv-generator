08d2f892c4b49df3f3e768e434e0428587ca518e
Brett Dorrans <hello@lapidist.net>
2024-03-05 18:12:49 +0000

    feat: rewrite CV to use XYZ format
    BREAKING CHANGE: new layout

--- src/copy.ts
+++ src/copy.ts
@@ -1,148 +1,141 @@
-export default {
+const HEADER = {
     name: 'Brett Dorrans',
-    title: 'Senior Front End Software Engineer',
-    intro: "With over a decade of professional software development experience, I've successfully led diverse teams in building high-quality products with a strong emphasis on accessibility, inclusive design, and research-led UX. My collaborative approach involves working closely with stakeholders of all levels to decipher requirements, align digital strategies, and contribute to business growth. Known for meticulous attention to detail, I advocate for best practices and implement automated code quality checks in CI to ensure codebase health. Beyond software development, I am dedicated to empowering teams, clients, and colleagues to reach their full potential by fostering a culture of excellence and delivering work to the highest standard. Throughout my career, I've effectively translated technical complexities into clear, actionable insights, playing a pivotal role in advancing company goals, building client relationships, and ensuring project success.",
-    sectionHeaders: {
-        experience: 'Experience',
-        contact: 'Contact'
+    contact: ['hello@lapidist.net', 'lapidist.net', 'github.com/bylapidist']
+};
+
+const SUMMARY = `With over 10 years of software development experience, I’ve led diverse teams in creating high-quality products with a focus on accessibility and inclusive design. I collaborate closely with stakeholders to decipher requirements, align strategies, and promote best practices, fostering a culture of excellence and delivering work to the highest standard.`;
+
+const SKILLS = [
+    {
+        title: 'Languages',
+        skills: ['JavaScript', 'TypeScript', 'PHP', 'C#']
     },
-    contact: {
-        email: 'hello@lapidist.net',
-        phone: '07944166857',
-        urls: [
-            'github.com/brettdorrans',
-            'github.com/bylapidist',
-            'linkedin.com/in/brettdorrans'
+    {
+        title: 'Frameworks',
+        skills: [
+            'React',
+            'Vue',
+            'Next',
+            'Gatsby',
+            'Symfony',
+            'Angular',
+            'ASP.NET'
         ]
     },
-    jobs: [
-        {
-            company: 'LendInvest',
-            positions: [
-                {
-                    title: 'Senior Software Engineer',
-                    from: 'Apr 2021',
-                    to: 'Nov 2023'
-                }
-            ],
-            description:
-                'Led a high-performing team of engineers developing design systems and software for a fintech specialising in mortgages. Promoted an inclusive team culture, prioritising accessibility and research-driven development. Actively involved in talent acquisition, advocating a people-centric approach. Managed a smooth migration of the design system from Flow to TypeScript, ensuring enhanced safety and consistency. Successfully championed the adoption of the design system, integrating shareable components, code, and design tokens for improved efficiency. Collaborated with stakeholders to align digital strategies with business objectives.',
-            technologies: [
-                'React',
-                'Redux',
-                'Javascript',
-                'Flow',
-                'Jest/RTL',
-                'Design Systems',
-                'Symfony',
-                'PHP8',
-                'OpenAPI',
-                'CI/CD'
-            ]
-        },
-        // {
-        //     company: 'Lapidist',
-        //     positions: [
-        //         {
-        //             title: 'Director',
-        //             from: 'Nov 2016',
-        //             to: 'Present'
-        //         }
-        //     ],
-        //     description:
-        //         'Produced open source software and provided contract development services.',
-        //     technologies: [
-        //         'React',
-        //         'Redux',
-        //         'Javascript',
-        //         'TypeScript',
-        //         'Jest',
-        //         'PHP8',
-        //         'Doctrine',
-        //         'Symfony',
-        //         'Java',
-        //         'CI/CD'
-        //     ]
-        // },
-        {
-            company: 'Golden Charter',
-            positions: [
-                {
-                    title: 'Software Developer',
-                    from: 'Jun 2020',
-                    to: 'Apr 2021'
-                },
-                {
-                    title: 'Software Consultant (Contract)',
-                    from: 'Feb 2020',
-                    to: 'Jun 2020'
-                }
-            ],
-            description:
-                'Developed software for the funeral industry within the Product and Innovation team. Transitioned products to a design system and component-based workflow. Enhanced code quality with CI tooling, boosted developer productivity by automating release cycles and adopting semver, and modernised legacy C# applications. Advocated for accessibility and inclusive design principles, mentoring junior team members on best practices.',
-            technologies: [
-                'Vue',
-                'Vuex',
-                'Jest',
-                'Javascript',
-                'PHP7',
-                'Doctrine',
-                'Symfony',
-                'C#',
-                'CI/CD'
-            ]
-        },
-        {
-            company: 'Markup',
-            positions: [
-                {
-                    title: 'Front-End Team Lead',
-                    from: 'Aug 2019',
-                    to: 'Dec 2019'
-                },
-                {
-                    title: 'Senior Software Engineer',
-                    from: 'Jan 2018',
-                    to: 'Dec 2019'
-                }
-            ],
-            description:
-                'Developed enterprise e-commerce software for top fashion brands, creating tooling, processes, and design methodology for design systems. Mentored junior developers through knowledge-sharing sessions. Scoped project requirements and regularly engaged with stakeholders in Stockholm, including designers, engineers, marketers, IT, and senior management. Advanced company goals, sold features, and enhanced client relations. Orchestrated sprints, set goals, and chaired agile rituals. Advocated for robust tooling with automated code quality checks in CI.',
-            technologies: [
-                'React',
-                'Redux',
-                'CSS Modules',
-                'Javascript',
-                'PHP7',
-                'Symfony',
-                'MySQL',
-                'CI/CD'
-            ]
-        },
-        {
-            company: 'Everyone',
-            positions: [
-                {
-                    title: 'Developer',
-                    from: 'Nov 2016',
-                    to: 'Dec 2017'
-                }
-            ],
-            description:
-                'Collaborated with designers, project managers, and clients on e-commerce and web applications. Created a cross-platform mobile app for the Go Industrial museum group. Scoped timescales and took ownership of complex project requirements to deliver robust, detail-oriented experiences on time and within budget.',
-            technologies: ['Angular2', 'Ionic', 'Javascript', 'PHP', 'SCSS']
-        },
-        {
-            company: 'Ibiza Digital Media',
-            positions: [
-                {
-                    title: 'Senior Digital Designer',
-                    from: 'Oct 2014',
-                    to: 'Sep 2016'
-                }
-            ],
-            description: null,
-            technologies: ['Javascript', 'PHP', 'SCSS']
-        }
-    ]
+    {
+        title: 'Styling',
+        skills: ['CSS', 'SCSS', 'CSS Modules', 'Styled Components', 'Tailwind']
+    },
+    {
+        title: 'Testing',
+        skills: ['Jest', 'React Testing Library', 'Cypress', 'Playwright']
+    },
+    {
+        title: 'Tooling',
+        skills: [
+            'Webpack',
+            'Babel',
+            'Vite',
+            'Parcel',
+            'Rollup',
+            'Redux',
+            'React Query',
+            'git',
+            'docker',
+            'Storybook'
+        ]
+    },
+    {
+        title: 'Back-End',
+        skills: ['Node', 'Express', 'REST', 'GraphQL', 'MySQL', 'Doctrine']
+    },
+    {
+        title: 'CI/CI',
+        skills: ['Jenkins', 'GitHub Actions', 'CircleCI', 'Azure']
+    }
+];
+
+const EXPERIENCES = [
+    {
+        title: 'Senior Software Engineer',
+        company: 'LendInvest',
+        location: 'London, United Kingdom (remote)',
+        from: '2021',
+        to: '2023',
+        bullets: [
+            'Led a high-performing team of talented engineers as the co-principal engineer of the design system team, developing processes and software for fintech within the specialist mortgage market.',
+            'Orchestrated seamless migration of design system from Flow and JavaScript to TypeScript, ensuring enhanced safety and consistency without any downtime.',
+            'Developed loan management software using a micro frontend architecture, ensuring efficient scalability.',
+            'Promoted the widespread use of the design system, incorporating reusable components and design tokens across multiple products, leading to a 75% decrease in loan application cost and 30% less rewritten code.',
+            'Collaborated closely with stakeholders to align digital strategies with business objectives, ensuring projects were delivered efficiently and to the highest standard.',
+            'Championed inclusive team culture, prioritising accessibility and research-driven development, to ensure team cohesion.',
+            'Engaged in talent acquisition, advocating for a people-centric approach valuing individuality, transparency and respect.',
+            'Led the effort to implement WCAG accessibility conformance, aspiring to AA and in some cases AAA standard.'
+        ]
+    },
+    {
+        title: 'Software Developer',
+        company: 'Golden Charter',
+        location: 'Glasgow, United Kingdom (remote)',
+        from: '2020',
+        to: '2021',
+        bullets: [
+            'Built software for the funeral industry using Vue, JavaScript, and C#, ensuring tailored solutions that met industry and regulatory compliance needs.',
+            'Transitioned products to a design system and component-based workflow, enhancing consistency and efficiency.',
+            'Enhanced code quality through CI tooling (Azure), boosting developer productivity via automated release cycles and adoption of a robust versioning strategy.',
+            'Modernised legacy C# applications, optimising performance and functionality for improved developer experience and future proofing.',
+            'Advocated for accessibility and inclusive design principles, fostering a more user-friendly and inclusive user experience.',
+            'Mentored junior team members on best practices, facilitating skill development and knowledge sharing.'
+        ]
+    },
+    {
+        title: 'Front End Team Lead',
+        company: 'Markup',
+        location: 'Glasgow, United Kingdom (hybrid)',
+        from: '2018',
+        to: '2019',
+        bullets: [
+            'Led a front-end engineering team for the GANT e-commerce site, ensuring successful project delivery and team cohesion.',
+            'Developed enterprise e-commerce software for leading fashion brands using React, JavaScript and Symfony, geared towards their brand-specific needs.',
+            'Mentored junior developers, fostering skill improvement and knowledge dissemination.',
+            'Aligned with company goals, effectively communicating features and promoting client relations.',
+            'Constructed sprints, set and followed through on goals, and chaired agile rituals to improve team efficiency and cohesion.',
+            'Developed a component library in Storybook and React, improving tooling, processes, design methodology, development velocity, and consistency.',
+            'Re-engineered the checkout flow, increasing conversion and reducing cart abandonment, leading to increased sales.'
+        ]
+    },
+    {
+        title: 'Developer',
+        company: 'Everyone',
+        location: 'Glasgow, United Kingdom',
+        from: '2016',
+        to: '2017',
+        bullets: [
+            'Assumed responsibility for managing intricate project needs and estimating timelines, resulting in the prompt and cost-effective delivery of thorough, detail-oriented experiences.',
+            'Created a cross-platform mobile app for the Go Industrial museum group, using Iconic and Angular, to guarantee seamless operation on all devices.',
+            'Engineered a social media platform for Craft Scotland, integrating job boards, profiles, e-commerce storefronts, and backend CMS tooling, demonstrating adeptness in creating multifaceted digital solutions.'
+        ]
+    },
+    {
+        title: 'Senior Digital Designer',
+        company: 'Ibiza Digital Media',
+        location: 'Glasgow, United Kingdom',
+        from: '2014',
+        to: '2016',
+        bullets: [
+            'Led a multidisciplinary team to create custom websites, web apps, and graphics for print, online, and social media.',
+            'Designed and developed a mobile-first e-commerce website for Eden Mill, boosting sales and improving online presence.',
+            'Facilitated skill development and efficiency through training sessions on internal software, Photoshop and Illustrator.',
+            'Developed an online magazine from concept to serving 85k users per month, amplifying brand visibility and engagement.',
+            'Ensured project success by working together with stakeholders at all levels to assess client requirements, plan communication, and suggest best practices for putting commercial ideas into action.',
+            'Managed full project lifecycle, including scoping project costings and timescales, ensuring timely and economical delivery.'
+        ]
+    }
+];
+
+export default {
+    HEADER,
+    SUMMARY,
+    SKILLS,
+    EXPERIENCES
 };
--- src/pdf.tsx
+++ src/pdf.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Document, Font, Page, Text, View } from '@react-pdf/renderer';
 import * as path from 'path';
-import Logo from './components/Logo';
-import Position from './components/Position';
 import copy from './copy';
 import { styles } from './styles';
 
@@ -34,57 +32,72 @@ const PDF = () => (
     <Document>
         <Page size="A4" style={styles.page}>
             <View style={styles.header}>
-                <Logo />
-                <Text style={styles.name}>{copy.name}</Text>
-                <Text style={styles.title}>{copy.title}</Text>
-            </View>
-            <View style={styles.section}>
-                <Text>{copy.intro}</Text>
-            </View>
-            <View style={styles.section}>
-                <Text style={styles.sectionHeader}>
-                    {copy.sectionHeaders.experience}
+                <Text style={styles.name}>{copy.HEADER.name}</Text>
+                <Text style={styles.contact}>
+                    {copy.HEADER.contact.join('  |  ')}
                 </Text>
-                {copy.jobs.map((job) => (
-                    <View key={job.company} style={styles.job}>
-                        {job.positions.map((position, index) => (
-                            <Position
-                                key={`${job.company} ${position.title}`}
-                                job={job}
-                                position={position}
-                                primary={index === 0}
-                            />
-                        ))}
-                        {job.description && (
-                            <Text style={styles.description}>
-                                {job.description}
-                            </Text>
-                        )}
-                        {job.technologies && (
-                            <Text style={styles.technologies}>
-                                <Text style={styles.technologiesHeader}>
-                                    Technologies:{' '}
-                                </Text>
-                                <Text>{job.technologies.join(', ')}</Text>
-                            </Text>
-                        )}
-                    </View>
+            </View>
+
+            <View style={styles.section}>
+                <Text style={styles.sectionHeader}>Skills</Text>
+                {copy.SKILLS.map((skill) => (
+                    <Text key={skill.title}>
+                        <Text style={styles.semibold}>{skill.title}: </Text>
+                        {skill.skills.join(', ')}
+                    </Text>
                 ))}
             </View>
+
             <View style={styles.section}>
-                <Text style={styles.sectionHeader}>
-                    {copy.sectionHeaders.contact}
-                </Text>
-                <View style={styles.contact}>
-                    <Text style={styles.primaryContact}>
-                        {`${copy.contact.email}\n${copy.contact.phone}`}
-                    </Text>
-                    <Text style={styles.secondaryContact}>
-                        {copy.contact.urls.map((url) => (
-                            <Text key={url}>{`${url}\n`}</Text>
-                        ))}
-                    </Text>
-                </View>
+                <Text style={styles.sectionHeader}>Experience</Text>
+                {copy.EXPERIENCES.map((experience) => (
+                    <div style={styles.experience} key={experience.company}>
+                        <div style={styles.experienceHeader}>
+                            <div style={styles.column}>
+                                <Text style={styles.semibold}>
+                                    {experience.title}
+                                </Text>
+                                <Text>, {experience.company} &ndash; </Text>
+                                <Text>{experience.location}</Text>
+                            </div>
+                            <div style={styles.column}>
+                                <Text>
+                                    {experience.from} &ndash; {experience.to}
+                                </Text>
+                            </div>
+                        </div>
+                        <ul style={styles.list}>
+                            {experience.bullets.map((item) => (
+                                <View
+                                    key={item}
+                                    style={{ flexDirection: 'row' }}
+                                >
+                                    <Text style={{ marginRight: 6 }}>
+                                        &bull;
+                                    </Text>
+                                    <Text>{item}</Text>
+                                </View>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </View>
+
+            <View style={styles.section}>
+                <Text style={styles.sectionHeader}>Education</Text>
+                <div style={styles.experience}>
+                    <div style={styles.experienceHeader}>
+                        <div style={styles.column}>
+                            <Text style={styles.semibold}>
+                                Edinburgh College of Art
+                            </Text>
+                            <Text>, BA (Hons) in Illustration</Text>
+                        </div>
+                        <div style={styles.column}>
+                            <Text>2007 &ndash; 2011</Text>
+                        </div>
+                    </div>
+                </div>
             </View>
         </Page>
     </Document>
